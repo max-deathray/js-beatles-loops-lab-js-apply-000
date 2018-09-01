1cac8bf6f74545a7e054577eb9ad8d322e5a283a
McRae Petrey <mpetrey@gmail.com>
2018-09-01 15:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+function theBeatlesPlay(musArray, insArray) {
+  const finalArray = [];
+  
+  for (let i = 0; i < musArray.length; i++) {
+    const currentBeatle = musArray[i];
+    let string = '';
+    string+currentBeatle + " plays " + insArray[i];
+    finalArray.push(string);
+  }
+  
+  return finalArray;
+}
